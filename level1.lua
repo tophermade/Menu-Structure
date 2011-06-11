4cbff8aa6d9800af9be2493be4335475c2375019
Topher <tophermade@gmail.com>
2011-06-10 21:43:22 -0600

    updated popup

--- level1.lua
+++ level1.lua
@@ -2,6 +2,8 @@ module(..., package.seeall)
 
 
 function new()	
+	level_name = "Friendly Level Name"
+	level_id = "level1" --should match lua file
 	
 	-- we store everything inside this group at the end
 	local localGroup = display.newGroup()		
@@ -9,23 +11,66 @@ function new()
 	-- change scene function
 	function changeScene(e)
 		if(e.phase == "ended") then
+		
+			local path = system.pathForFile( "loadlast.txt", system.DocumentsDirectory )
+ 
+			-- io.open opens a file at path. returns nil if no file found
+			local file = io.open( path, "r" )
+			if file then
+			   file = io.open( path, "w" )
+			   file:write(level_id)
+			   io.close( file )
+			else
+			   -- create file b/c it doesn't exist yet
+			   file = io.open( path, "w" )
+			   file:write(level_id)
+			   io.close( file )
+			end
+		
 			director:changeScene(e.target.scene, "moveFromRight");
 		end
 	end
 	
+----
 	-- we want the game object to be available here
+	-- level & game code
+----	
 	game = require("game")
 	
-	-- close the menu if unpausing
-	function closeMenu(e)
-		transition.to(level_menu, {delay=200, time=500, alpha=0});
-	end
+	
+
+	
+----
+	-- //we want the game object to be available here
+	-- //level & game code
+----		
 	
 	-- pop up menu with options to reset, load main menu, etc
 	local level_menu = display.newGroup()
 		level_menu:setReferencePoint(display.CenterReferencePoint);
 		level_menu.alpha = 0;
-				
+		
+		
+	-- pop menu function
+	function popMenu(e)
+		-- you'll want to freeze everything ...
+		
+		-- and then load the pop 
+		transition.to(level_menu, {delay=200, time=500, alpha=1});	
+	end	
+		
+	-- close the menu if unpausing
+	function closeMenu(e)
+		transition.to(level_menu, {delay=200, time=500, alpha=0});	
+	end
+	
+	local btn_level_menu = display.newText("Level Options", 0, 0, native.systemFontBold, 16);
+		btn_level_menu:setReferencePoint(display.CenterReferencePoint);
+		btn_level_menu.x = _w/2;
+		btn_level_menu.y = _h-45;
+		btn_level_menu.scene = "apps";
+		btn_level_menu:addEventListener("touch", popMenu);	
+		
 	local black_out = display.newRect(0,0,_w,_h);	
 		black_out:setFillColor(0,0,0);
 		black_out.alpha = .8;
@@ -48,36 +93,17 @@ function new()
 		level_menu_btn_reset:setReferencePoint(display.CenterReferencePoint);
 		level_menu_btn_reset.x = _w/2;
 		level_menu_btn_reset.y = _h/2;
-		level_menu_btn_reset.scene = "loadlevel1";
+		level_menu_btn_reset.scene = "reloader";
 		level_menu:insert(level_menu_btn_reset);
-		level_menu_btn_reset:addEventListener("touch", changeScene);	
-	
+		level_menu_btn_reset:addEventListener("touch", changeScene);			
+		
 	local level_menu_btn_play = display.newText("Continue Playing", 0, 0, native.systemFontBold, 16);
 		level_menu_btn_play:setReferencePoint(display.CenterReferencePoint);
 		level_menu_btn_play.x = _w/2;
 		level_menu_btn_play.y = _h/2+45;
 		level_menu:insert(level_menu_btn_play);
 		level_menu_btn_play:addEventListener("touch", closeMenu);	
-	
-	-- pop menu function
-	function popMenu(e)
-		-- you'll want to freeze everything ...
-		
-		-- and then load the pop 
-		transition.to(level_menu, {delay=200, time=500, alpha=1});	
-	end
-	
-	
-		
-	
-	-- menu buttons	
-		
-	local btn_level_menu = display.newText("Level Options", 0, 0, native.systemFontBold, 16);
-		btn_level_menu:setReferencePoint(display.CenterReferencePoint);
-		btn_level_menu.x = _w/2;
-		btn_level_menu.y = _h/2+45;
-		btn_level_menu.scene = "apps";
-		btn_level_menu:addEventListener("touch", popMenu);
+
 
 	
 	--- insert everything into the localGroup	
